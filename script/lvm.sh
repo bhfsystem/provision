04b72317ec5acc9ef488018bd3c5b1e2aefbbd8b
Tom Bombadil <amanibhavam@destructuring.org>
2016-10-12 01:37:04 -0700

    dont mess with docker.service

--- script/lvm.sh
+++ script/lvm.sh
@@ -3,7 +3,6 @@
 function main {
   lvreduce -f -L 1M inception/placeholder
   lvcreate -l '50%FREE' -n lxd inception
-  lvcreate -T -l '50%FREE' inception/docker --poolmetadatasize 32M -c 1M || true
 }
 
 main "$@"
